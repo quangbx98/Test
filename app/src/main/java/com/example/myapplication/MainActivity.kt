f66d9ddc22b0c3bef22ef4f723d1050b9688ff2d
quangbx <quangbx@ominext.com>
2019-09-18 17:46:37 +0700

    test camera

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val CAMERA_REQUEST = 1888
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+
+    }
+
+    private fun initView() {
+        button1.setOnClickListener {
+            val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, CAMERA_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === CAMERA_REQUEST) {
+            val photo = data?.extras?.get("data")
+            if (photo != null) {
+                photo as Bitmap
+                imageView1.setImageBitmap(photo)
+            }else return
+        }
     }
 }

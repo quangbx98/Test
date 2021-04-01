35f330c407cd0ead0b4bac9d9641d5999455ea34
Quangbx <quangbx@ominext.com>
2021-04-01 15:20:52 +0700

    Config

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {

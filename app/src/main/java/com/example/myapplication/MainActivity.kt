12fe1dbbf9b4d2718bd23f71bc1d5cc957738ef2
Quangbx <quangbx@ominext.com>
2021-04-02 09:47:34 +0700

    update config

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initView()
-
+        println("quangbx")
     }
 
     private fun initView() {
